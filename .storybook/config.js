8ba162dec225d0dac28069f8225053b91a6f5607
Gregory Desfour <gouegd@users.noreply.github.com>
2019-04-24 23:16:52 +1200

    build(storybook): Add background addon to stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+
+addParameters({
+  backgrounds: [
+    { name: 'twitter', value: '#00aced', default: true },
+    { name: 'facebook', value: '#3b5998' },
+  ],
+});
+
 
 // automatically import all src files ending in *.stories.js
 const req = require.context('../src', true, /\.stories\.js$/);
