4e410bef0aaeb84410952f58f8f453ceaf257381
dore <dore_chen@hotmail.com>
2019-07-03 18:27:47 +1200

    wip

--- src/DatedInput.js
+++ src/DatedInput.js
@@ -1,46 +1,76 @@
-import { jsx, css } from '@emotion/core'
-import T from 'prop-types'
-import moment from 'moment'
+// import { jsx, css } from '@emotion/core'
+// import { useState } from 'react'
+// import T from 'prop-types'
+// import moment from 'moment'
 
-import { TextArea } from './TextArea'
+// import { TextArea } from './TextArea'
+// import { Icon } from './Icon'
 
-const baseDatedInput = css`
-  position: relative;
-  display: flex;
-  width: 100%;
-  border: none;
-  font-size: 24px;
-  text-align: center;
-  align-items: stretch;
-  @media screen and (max-width: 600px) {
-    font-size: 16px;
-  }
-`
+// const baseDatedInput = css`
+//   position: relative;
+//   display: flex;
+//   width: 100%;
+//   border: none;
+//   font-size: 24px;
+//   text-align: center;
+//   align-items: stretch;
+//   /* margin-bottom: 2px;
+//     @include mobile {
+//       margin-bottom: 16px;
+//     }
+//     &:first-child {
+//       margin-top: 2px;
+//       @include mobile {
+//         margin-top: 16px;
+//       }
+//     }
+//     &:last-child {
+//       margin-bottom: 0px;
+//     } */
+//   @media screen and (max-width: 600px) {
+//     font-size: 16px;
+//   }
+// `
 
-const baseDate = css`
-  display: flex;
-  flex-grow: 0;
-  flex-basis: 10rem;
-  margin-right: 2px;
-  background-color: #fafafa;
-  font-size: 21px;
-  align-items: center;
-  justify-content: center;
-  @media screen and (max-width: 600px) {
-    font-size: 16px;
-  }
-`
+// const baseDate = css`
+//   display: flex;
+//   flex-grow: 0;
+//   flex-basis: 10rem;
+//   margin-right: 2px;
+//   background-color: #fafafa;
+//   font-size: 21px;
+//   align-items: center;
+//   justify-content: center;
+//   @media screen and (max-width: 600px) {
+//     font-size: 16px;
+//   }
+// `
 
-export const DatedInput = ({ date, value, ...props }) => {
-  return (
-    <div css={[baseDatedInput]}>
-      <span css={[baseDate]}>{moment(date).format('DD/MM/YY')}</span>
-      <TextArea defaultValue={value} {...props} />
-    </div>
-  )
-}
+// const input = css`
+//   /* margin: 0; */
+//   /* text-align: left; */
+// `
 
-DatedInput.propTypes = {
-  date: T.date,
-  value: T.object,
-}
+// export const DatedInput = ({ date, value, onChange, onDelete, ...props }) => {
+//   const [inputValue, setInputValue] = useState(value)
+//   return (
+//     <div css={[baseDatedInput]}>
+//       <span css={[baseDate]}>{moment(date).format('DD/MM/YY')}</span>
+//       <TextArea
+//         css={[input]}
+//         onChange={e => setInputValue(e.target.value)}
+//         onBlur={() => ""}
+//       >
+//         {inputValue || props.children}
+//       </TextArea>
+//       {onDelete && <Icon light onDelete={onDelete}>✕</Icon>}
+//     </div>
+//   )
+// }
+
+// DatedInput.propTypes = {
+//   date: T.date,
+//   value: T.object,
+//   onChange: T.func,
+//   onDelete: T.func,
+// }
--- src/Input.stories.js
+++ src/Input.stories.js
@@ -7,6 +7,7 @@ import inputNotes from './inputNotes.md'
 import textAreaNotes from './textAreaNotes.md'
 
 import { Input } from './Input'
+import { DatedInput } from './DatedInput'
 import { TextArea } from './TextArea'
 
 const example = 'Text area\nput notes here!\n:)'
@@ -39,6 +40,26 @@ storiesOf('Input Field', module)
       </div>
     ),
     { notes: { markdown: inputNotes } }
+  )
+  .add(
+    'Input Groups',
+    () => (
+      <div>
+        <h1>t</h1>
+        <DatedInput
+          // onClick={action('text area selected')}
+          // placeholder="Text area with placeholder"
+          date={new Date()}
+          value="hello there"
+          // onDelete={() => console.log('delete')}
+        />
+        {/* <DatedInput
+          date={new Date()}
+          value="more"
+        /> */}
+      </div>
+    ),
+    { notes: 'testing' }
   )
   .add(
     'TextArea',
