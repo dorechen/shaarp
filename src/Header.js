b868781b0b6e78995aa79f95d8e9c257c211dabb
dore <dore_chen@hotmail.com>
2019-07-04 13:51:40 +1200

    feat(Header): add h2 component
    and adjust h1 styling

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,6 @@ const base = css`
   box-sizing: border-box;
   width: 100%;
   color: #000000;
-  font-family: 'inherit';
   text-align: center;
   align-items: center;
 `
@@ -21,10 +20,27 @@ const h1Size = css`
   }
 `
 
+const h2Size = css`
+  height: 33px;
+  font-size: 24px;
+
+  @media screen and (max-width: 600px) {
+    font-size: 16px;
+  }
+`
+
 export const H1 = ({ ref, ...props }) => {
   return <h1 css={[base, h1Size]} ref={ref} {...props} />
 }
 
+export const H2 = ({ ref, ...props }) => {
+  return <h2 css={[base, h2Size]} ref={ref} {...props} />
+}
+
 H1.propTypes = {
+  ref: T.object,
+}
+
+H2.propTypes = {
   ref: T.object,
 }
